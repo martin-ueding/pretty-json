477e13f07bc436f1f3cd188de38db7e55da9561f
Martin Ueding <dev@martin-ueding.de>
2012-02-12 21:08:52 +0100

    Init

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
+
+from distutils.core import setup
+
+setup(
+    author = "Martin Ueding",
+    author_email = "dev@martin-ueding.de",
+    name = "prettyjson",
+    scripts = ["pretty-json"],
+)
