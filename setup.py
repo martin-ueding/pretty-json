4e336e8b202113f6c551457f16612604e0cf4c71
Martin Ueding <no-reply@martin-ueding.de>
2020-02-07 11:33:46 +0100

    Remove now discontinued email address

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
+# Copyright © 2012 Martin Ueding <martin-ueding.de>
 
 from distutils.core import setup
 
 setup(
     author = "Martin Ueding",
-    author_email = "dev@martin-ueding.de",
     name = "prettyjson",
     scripts = ["pretty-json"],
 )
